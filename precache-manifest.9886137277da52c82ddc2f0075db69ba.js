1650eb0ea20b3b7f3b3b807fe4a88efba6773946
Makhmut <grupry@gmail.com>
2021-04-21 15:56:07 +0300

    Updates

---
+++ precache-manifest.9886137277da52c82ddc2f0075db69ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75922ed9572bdf266b9a972c9aeb11d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "63a357aac79d27078e4e",
+    "url": "./static/css/main.e73d41c8.chunk.css"
+  },
+  {
+    "revision": "6658b11d6bda10e24993",
+    "url": "./static/js/2.695925bf.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.695925bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63a357aac79d27078e4e",
+    "url": "./static/js/main.8d91e01a.chunk.js"
+  },
+  {
+    "revision": "0c165204287428b65967",
+    "url": "./static/js/runtime-main.a37292e9.js"
+  }
+]);
